462fe0de58d26aff1429aeb24dc9cf52db4da30c
BuildTools <unconfigured@null.spigotmc.org>
2020-05-15 00:05:37 +0200

    Teacher repository added and Teacher classes getter, setter and constructor added

---
+++ src/main/java/at/htl/xam/controller/TeacherRepository.java
@@ -0,0 +1,135 @@
+package at.htl.xam.controller;
+
+import at.htl.xam.model.Question;
+import at.htl.xam.model.Teacher;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeacherRepository {
+    private TeacherRepository teacherRepository = new TeacherRepository();
+
+    public TeacherRepository() {
+        if (!tableExists()) {
+            createTable();
+        }
+    }
+
+    public void createTable() {
+        try (Connection connection = DatasourceFactory.getDataSource().getConnection()) {
+
+            String sql = "CREATE TABLE Teacher(" +
+                    "teacher_id int NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)," +
+                    "name varchar(50) NOT NULL," +
+                    "username varchar(50) NOT NULL," +
+                    "password varchar(32) NOT NULL," +
+                    "CONSTRAINT PK_Teacher PRIMARY KEY (teacher_id)" +
+                    ")";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.execute();
+            System.out.println("Created table Teacher.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean tableExists() {
+        try (Connection conn = DatasourceFactory.getDataSource().getConnection()) {
+
+            String sql = "SELECT * FROM SYS.SYSTABLES WHERE TABLENAME = 'Teacher'";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet result = pstmt.executeQuery();
+
+            if (result.next()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    public void addTeacher(Teacher teacher) {
+        try (Connection connection = DatasourceFactory.getDataSource().getConnection()) {
+
+            // TODO: Insert teacher in database
+            String sql = "INSERT INTO Teacher(name, username, password) VALUES(?, ?, ?)";
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, teacher.getName());
+            pstmt.setString(2, teacher.getUsername());
+            pstmt.setString(3, teacher.getPassword());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public List<Teacher> getAllTeachers() {
+        List<Teacher> teachers = teacherRepository.getAllTeachers();
+
+        try (Connection connection = DatasourceFactory.getDataSource().getConnection()) {
+
+            // TODO: Select all teachers from database
+            String sql = "SELECT teacher_id, name, username, password FROM Teacher";
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            ResultSet result = pstmt.executeQuery();
+
+            while(result.next()){
+                int id = result.getInt("question_id");
+                String name = result.getString("name");
+                String username = result.getString("username");
+                String password = result.getString("password");
+                teachers.add(new Teacher(id, name, username, password));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return teachers;
+    }
+
+
+    public void removeTeacher(Teacher teacher) {
+        try (Connection connection = DatasourceFactory.getDataSource().getConnection()) {
+
+            // TODO: Delete teacher from database
+            String sql = "DELETE FROM Teacher WHERE teacher_id = ?";
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setInt(1, teacher.getId());
+
+            pstmt.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void saveTeacher(Teacher teacher) {
+        try (Connection connection = DatasourceFactory.getDataSource().getConnection()) {
+
+            // TODO: Update question in database
+            String sql = "UPDATE Teacher SET teacher_id = ?, name = ?, username = ?, password = ?,  WHERE CATEGORY_ID = ?";
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setInt(1, teacher.getId());
+            pstmt.setString(2, teacher.getName());
+            pstmt.setString(3, teacher.getUsername());
+            pstmt.setInt(4, teacher.getId());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
--- src/main/java/at/htl/xam/model/Teacher.java
+++ src/main/java/at/htl/xam/model/Teacher.java
@@ -2,6 +2,37 @@ package at.htl.xam.model;
 
 public class Teacher extends Person {
 
-    String name;
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+
+    public Teacher() {
+    }
+
+    public Teacher(int id, String name, String username, String password) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
 
 }
