bb96a50bce4347092a912d312e73575be8bf5f99
FitzingerJ <jacki.fitzinger@gmail.com>
2020-05-18 12:13:53 +0200

    Student.java updated

--- src/main/java/at/htl/xam/model/Student.java
+++ src/main/java/at/htl/xam/model/Student.java
@@ -1,7 +1,49 @@
 package at.htl.xam.model;
 
-public class Student extends Person {
+public class Student{
 
-    String klasse;
+    long id;
+    String username;
+    String password;
+    String classRoom;
 
+    public Student() {
+    }
+
+    public Student(long id, String username, String password, String classRoom) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.classRoom = classRoom;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getClassRoom() {
+        return classRoom;
+    }
+
+    public void setClassRoom(String classRoom) {
+        this.classRoom = classRoom;
+    }
 }
+
+
