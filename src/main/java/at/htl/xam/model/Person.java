ef297093a0e0646df76f1ea85ace01bd41fee789
merisbsc <m.besic@students.htl-leonding.ac.at>
2020-05-18 12:09:51 +0200

    modified Person.java

--- src/main/java/at/htl/xam/model/Person.java
+++ src/main/java/at/htl/xam/model/Person.java
@@ -2,10 +2,44 @@ package at.htl.xam.model;
 
 public class Person {
 
-    int id;
+    Long id;
     String username;
     String password; //verschlüsselt speichern
     String type; //Teacher oder Student
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
 
 
 }
