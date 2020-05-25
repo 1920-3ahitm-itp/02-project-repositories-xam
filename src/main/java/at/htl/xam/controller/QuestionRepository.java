38efc92f4e32b0b6a6d16d2338294580e7430e5c
merisbsc <m.besic@students.htl-leonding.ac.at>
2020-05-25 12:49:47 +0200

    deleted QuestionRepository tableexists and createtable

--- src/main/java/at/htl/xam/controller/QuestionRepository.java
+++ src/main/java/at/htl/xam/controller/QuestionRepository.java
@@ -10,65 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class QuestionRepository {
-    //private QuestionRepository questionRepository = new QuestionRepository();
-
-//    public QuestionRepository() {
-//        if (!tableExists()) {
-//            createTable();
-//        }
-//    }
-
-//    public void createTable() {
-//        try (Connection connection = DatasourceFactory.getDataSource().getConnection()) {
-//
-//            String sql = "CREATE TABLE Question(" +
-//                    "question_id int NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)," +
-//                    "headline varchar(100)," +
-//                    "description varchar(1000)," +
-//                    "result varchar(100) NOT NULL," +
-//                    "quiz_id int not null," +
-//                    "CONSTRAINT PK_Question PRIMARY KEY (question_id)" +
-//                    "CONSTRAINT FK_Question FOREIGN KEY (quiz_id) references Quiz(quiz_id)" +
-//                    ")";
-//
-//            PreparedStatement statement = connection.prepareStatement(sql);
-//            statement.execute();
-//            System.out.println("Created table Question.");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-    public void dropTable() {
-        try (Connection connection = DatasourceFactory.getDataSource().getConnection()) {
-
-            String sql = "DROP TABLE Question";
-
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.execute();
-            System.out.println("Dropped table Question.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public boolean tableExists() {
-        try (Connection conn = DatasourceFactory.getDataSource().getConnection()) {
-
-            String sql = "SELECT * FROM SYS.SYSTABLES WHERE TABLENAME = 'Question'";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            ResultSet result = pstmt.executeQuery();
-
-            if (result.next()) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
 
     public void addQuestion(Question question) {
         try (Connection connection = DatasourceFactory.getDataSource().getConnection()) {
@@ -76,9 +17,9 @@ public class QuestionRepository {
             // TODO: Insert question in database
             String sql = "INSERT INTO Question(headline, description, result, QUIZ_ID) VALUES(?, ?, ?, ?)";
             PreparedStatement pstmt = connection.prepareStatement(sql);
-            pstmt.setString(1, question.getHeadline());
-            pstmt.setString(2, question.getDesc());
-            pstmt.setString(3, question.getResult());
+            pstmt.setString(1, question.getQueHeadline());
+            pstmt.setString(2, question.getQueDesc());
+            pstmt.setString(3, question.getQueResult());
             pstmt.setLong(4, 1);
 
             pstmt.execute();
@@ -133,10 +74,10 @@ public class QuestionRepository {
             // TODO: Update question in database
             String sql = "UPDATE Question SET headline = ?, description = ?, result = ? WHERE question_id = ?";
             PreparedStatement pstmt = connection.prepareStatement(sql);
-            pstmt.setString(1, question.getHeadline());
-            pstmt.setString(2, question.getDesc());
-            pstmt.setString(3, question.getResult());
-            pstmt.setLong(4, question.getId());
+            pstmt.setString(1, question.getQueHeadline());
+            pstmt.setString(2, question.getQueDesc());
+            pstmt.setString(3, question.getQueResult());
+            pstmt.setLong(4, question.getQueId());
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -151,10 +92,10 @@ public class QuestionRepository {
 
             try (ResultSet rs = preparedStatement.executeQuery()) {
                 Question q = new Question();
-                q.setId(id);
+                q.setQueId(id);
                 rs.next();
                 String headline = rs.getString("headline");
-                q.setHeadline(headline);
+                q.setQueHeadline(headline);
                 return q;
             }
         } catch (SQLException throwables) {
